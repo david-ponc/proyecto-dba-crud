e8240cecd0a6aa921371f7d90e9dc9337796c938
David Ponce Vargas <buap.david@gmail.com>
2020-11-21 15:49:01 -0600

    response implementation

--- src/controllers/CardsPutController.js
+++ src/controllers/CardsPutController.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const { success, error } = require('../helpers/response')
 const MongoService = require('../services/mongoService')
 const router = express.Router()
 
-router.put('/put/cards/:cardId', async (req, res) => {
+router.put('/put/cards/:cardId', (req, res) => {
   const { cardId } = req.params
   const { body } = req
   const mongo = new MongoService()
-  const result = await mongo.updateCard(cardId, body)
-  res.send({ body: result })
+  mongo.updateCard(cardId, body)
+    .then(result => success(req, res, result))
+    .catch(err => error(req, res, err))
 })
 
 module.exports = router
