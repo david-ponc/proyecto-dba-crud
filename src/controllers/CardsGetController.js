bcaf64dfd967bdccd70c39f72093f9ae06f048de
Marco Pacheco <pacheco.marco@hotmail.com>
2020-11-21 17:11:14 -0600

    Get all cards

--- src/controllers/CardsGetController.js
+++ src/controllers/CardsGetController.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const MongoService = require('../services/mongoService')
 const router = express.Router()
 
-router.get('/', (req, res) => { res.send({message: 'Funciona'}) })
+router.get('/', async(req, res) => { 
+    const mongo = new MongoService()
+    const db = await mongo.connect();
+    const collection = db.collection("cards");
+    const result = await collection.find().toArray();
+    res.send(result)
+})
 
 module.exports = router
