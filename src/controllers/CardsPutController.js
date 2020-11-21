fc0cf658ca8c21d14a0d73cc8f457167fd6b407d
David Ponce Vargas <buap.david@gmail.com>
2020-11-21 15:37:40 -0600

    operation: update complete

--- src/controllers/CardsPutController.js
+++ src/controllers/CardsPutController.js
@@ -2,11 +2,12 @@ const express = require('express')
 const MongoService = require('../services/mongoService')
 const router = express.Router()
 
-router.put('/', async (req, res) => {
+router.put('/put/cards/:cardId', async (req, res) => {
+  const { cardId } = req.params
+  const { body } = req
   const mongo = new MongoService()
-  const db = await mongo.connect()
-  // const result = db.editCard()
-  res.send({body: 'Funciona'})
+  const result = await mongo.updateCard(cardId, body)
+  res.send({ body: result })
 })
 
 module.exports = router
