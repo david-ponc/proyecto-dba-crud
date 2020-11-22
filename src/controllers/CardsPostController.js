28f907eb958e65dfdc687590ba78796ec91f596c
Tony-coder17 <71418084+Tony-coder17@users.noreply.github.com>
2020-11-21 22:41:01 -0600

    prueba

--- src/controllers/CardsPostController.js
+++ src/controllers/CardsPostController.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const MongoService = require('../services/mongoService')
+const { success, error } = require('../helpers/response')
 
-router.post('/', (req, res) => { res.send({message: 'Funciona'}) })
+router.post('/', (req, res) => { 
+    const mongo = new MongoService()
+
+    mongo.insertCards()
+    .then(result => success(req, res, result))
+    .catch(err => error(req, res, err))
+})
 
 module.exports = router
